45caeb8e77066a01a8d38df10fbef3b95f397bc2
bavtim <40119744+bavtim@users.noreply.github.com>
2023-11-04 09:28:01 +0300

    update

--- main.py
+++ main.py
@@ -62,54 +62,82 @@ async def handle_help(message: types.Message):
 async def echo_message(message: types.Message):
 
     rawData=DBcontrol.getDBdataUsers(message.chat.id)
+    print(rawData)
     str_temp=[]
     for i in range(len(rawData)):
         row=rawData[i]
-        s = f"{i}\)"
-        s += f"pSQL_dbname:{row['pSQL_dbname']}\n"
-        s += f"pSQL_dbuser:{row['pSQL_dbuser']}\n"
-        s += f"pSQL_dbpassword:{row['pSQL_dbpassword']}\n"
-        s += f"pSQL_dbhost:{row['pSQL_dbhost']}\n"
+        s = f"БД № {i}\n"
+        s += f"pSQL_dbname: {row['pSQL_dbname']}\n"
+        s += f"pSQL_dbuser: {row['pSQL_dbuser']}\n"
+        s += f"pSQL_dbpassword: {row['pSQL_dbpassword']}\n"
+        s += f"pSQL_dbhost: {row['pSQL_dbhost']}\n"
         str_temp.append(s)
+
+
     text = markdown.text(
         markdown.markdown_decoration.quote(f"Количество указанных БД: {len(rawData)}"),
         markdown.text(
-            markdown.underline( "Список БД пуст" if len(rawData)==0 else "Перечень БД:"),
+            "Список БД пуст" if len(rawData)==0 else "Перечень БД:",
             *str_temp,
+            sep="\n"
         ),
             sep="\n"
         )
 
     await message.answer(
         text=text,
-        parse_mode=ParseMode.MARKDOWN_V2,
     )
 @dp.message(F.text[:len( "Добавить БД")] == "Добавить БД")
 async def echo_message(message: types.Message):
-
-    rawData=DBcontrol.getDBdataUsers(message.chat.id)
-    str_temp=[]
-    for i in range(len(rawData)):
-        row=rawData[i]
-        s = f"{i}\)"
-        s += f"pSQL_dbname:{row['pSQL_dbname']}\n"
-        s += f"pSQL_dbuser:{row['pSQL_dbuser']}\n"
-        s += f"pSQL_dbpassword:{row['pSQL_dbpassword']}\n"
-        s += f"pSQL_dbhost:{row['pSQL_dbhost']}\n"
-        str_temp.append(s)
-    text = markdown.text(
-        markdown.markdown_decoration.quote(f"Количество указанных БД: {len(rawData)}"),
-        markdown.text(
-            markdown.underline( "Список БД пуст" if len(rawData)==0 else "Перечень БД:"),
-            *str_temp,
-        ),
-            sep="\n"
+    strip_s=message.text[len( "Добавить БД"):].split()
+    if len(strip_s)==4:
+        try:
+            s = f"Аргументы\n"
+            s += f"pSQL_dbname: {strip_s[0]}\n"
+            s += f"pSQL_dbuser: {strip_s[1]}\n"
+            s += f"pSQL_dbpassword: {strip_s[2]}\n"
+            s += f"pSQL_dbhost: {strip_s[3]}\n"
+            await message.answer(
+                text=s,
+            )
+            DBcontrol.add_user_info(message.chat.id,strip_s[0],strip_s[1],strip_s[2],strip_s[3])
+            await message.answer(
+                text="Успешно добавлена база данных, ожидайте информацию о подключении",
+            )
+        except:
+            await message.answer(
+                text="Произошла ошибка при добавлении",
+            )
+    else:
+        await message.answer(
+             text="Введите сообщение в формате:\n\"Добавить БД pSQL_dbname pSQL_dbuser pSQL_dbpassword pSQL_dbhost\",\nНапример, \"Добавить БД testDB TGbot 12345 0.0.0.0:8888\"",
         )
 
-    await message.answer(
-        text=text,
-        parse_mode=ParseMode.MARKDOWN_V2,
-    )
+
+    print( strip_s)
+    # rawData=DBcontrol.getDBdataUsers(message.chat.id)
+    # str_temp=[]
+    # for i in range(len(rawData)):
+    #     row=rawData[i]
+    #     s = f"{i}\)"
+    #     s += f"pSQL_dbname:{row['pSQL_dbname']}\n"
+    #     s += f"pSQL_dbuser:{row['pSQL_dbuser']}\n"
+    #     s += f"pSQL_dbpassword:{row['pSQL_dbpassword']}\n"
+    #     s += f"pSQL_dbhost:{row['pSQL_dbhost']}\n"
+    #     str_temp.append(s)
+    # text = markdown.text(
+    #     markdown.markdown_decoration.quote(f"Количество указанных БД: {len(rawData)}"),
+    #     markdown.text(
+    #         markdown.underline( "Список БД пуст" if len(rawData)==0 else "Перечень БД:"),
+    #         *str_temp,
+    #     ),
+    #         sep="\n"
+    #     )
+    #
+    # await message.answer(
+    #     text=text,
+    #     parse_mode=ParseMode.MARKDOWN_V2,
+    # )
 @dp.message(F.text == "Работа с БД")
 async def echo_message(message: types.Message):
     kb = [[types.KeyboardButton(text="Получить список БД")],[types.KeyboardButton(text="Добавить БД")]]
